ab16a9cb9ec137f3367ab7dc634748584635f4ef
Dylan DesRosier <dylan@pooltogether.com>
2021-02-09 12:59:49 -0500

    fix react-hook-form use

--- lib/components/proposals/ProposalCreationForm.jsx
+++ lib/components/proposals/ProposalCreationForm.jsx
@@ -6,41 +6,27 @@ import { TextInputGroup } from 'lib/components/TextInputGroup'
 import { useUserCanCreateProposal } from 'lib/hooks/useUserCanCreateProposal'
 import React from 'react'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, useWatch } from 'react-hook-form'
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 
 export const ProposalCreationForm = () => {
   const { userCanCreateProposal } = useUserCanCreateProposal()
-  const { handleSubmit, register, watch, formState } = useForm({ mode: 'all' })
+  const { handleSubmit, register, watch, formState, control } = useForm({ mode: 'all' })
 
   const [actions, setActions] = useState([])
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
 
   return (
     <>
       <ActionsCard />
-      <TitleCard
-        title={title}
-        setTitle={setTitle}
-        register={register}
-        disabled={!userCanCreateProposal}
-      />
-      <DescriptionCard
-        description={description}
-        setDescription={setDescription}
-        disabled={!userCanCreateProposal}
-      />
+      <TitleCard register={register} disabled={!userCanCreateProposal} />
+      <DescriptionCard register={register} control={control} disabled={!userCanCreateProposal} />
     </>
   )
 }
 
 const TitleCard = (props) => {
-  const { title, setTitle: setParentTitle, register, disabled } = props
-
-  const [localTitle, setLocalTitle] = useState(title)
-  const setTitleDebounced = useDebouncedFn(setParentTitle, 200, null, [setParentTitle])
+  const { register, disabled } = props
 
   return (
     <Card className='text-accent-1'>
@@ -53,32 +39,17 @@ const TitleCard = (props) => {
         disabled={disabled}
         placeholder='Enter the title of your proposal'
         id='_proposalTitle'
-        name='Proposal Title'
         label='Proposal title'
+        name='title'
         required
         register={register}
-        onChange={(e) => {
-          setLocalTitle(e.target.value)
-          setTitleDebounced(e.target.value)
-        }}
-        value={localTitle}
       />
     </Card>
   )
 }
 
 const DescriptionCard = (props) => {
-  const { description, setDescription: setParentDescription, register, disabled } = props
-
-  const [localDescription, setLocalDescription] = useState(description)
-  const setDescriptionDebounced = useDebouncedFn(setParentDescription, 200, null, [
-    setParentDescription
-  ])
-
-  const setDescription = (description) => {
-    setLocalDescription(description)
-    setDescriptionDebounced(description)
-  }
+  const { control, register, disabled } = props
 
   return (
     <Card className='text-accent-1'>
@@ -88,30 +59,31 @@ const DescriptionCard = (props) => {
         This is where voters will educate themselves on what they are voting on.
       </p>
       <MarkdownInputArea
-        text={localDescription}
-        setText={setDescription}
+        name='description'
+        control={control}
         register={register}
         disabled={disabled}
-        name='Proposal Description'
       />
     </Card>
   )
 }
 
 const MarkdownInputArea = (props) => {
-  const { text, setText, register, disabled } = props
+  const { control, name, register, disabled } = props
+
+  const text = useWatch({ control, name, defaultValue: '' })
 
   const tabs = [
     {
       title: 'Write',
       view: (
         <TextArea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
           placeholder='Add the proposal details here'
-          register={register}
           rows={15}
           disabled={disabled}
+          name='description'
+          required
+          register={register}
         />
       )
     },
@@ -125,12 +97,12 @@ const MarkdownInputArea = (props) => {
 }
 
 const TextArea = (props) => {
-  const { register, text, setText, classNames, ...textAreaProps } = props
+  const { register, required, pattern, validate, classNames, ...textAreaProps } = props
   return (
     <textarea
       className={classnames('p-1 bg-body text-inverse w-full resize-none', classNames)}
+      ref={register({ required, pattern, validate })}
       {...textAreaProps}
-      ref={register}
     />
   )
 }
@@ -163,7 +135,9 @@ const TabbedView = (props) => {
         ))}
       </div>
       <div className='bg-body p-8 border border-accent-3 rounded-b-lg'>
-        {tabs[selectedTabIndex].view}
+        {tabs.map((tab, index) => (
+          <div className={classnames({ hidden: selectedTabIndex !== index })}>{tab.view}</div>
+        ))}
       </div>
     </>
   )
