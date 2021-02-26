05a5e09a1c1590b3b637a364e5ff1c0e174c4e69
Dylan DesRosier <dylan@pooltogether.com>
2021-02-26 14:43:58 -0500

    self review

--- lib/constants.jsx
+++ lib/constants.jsx
@@ -62,8 +62,7 @@ export const QUERY_KEYS = {
   tokenFaucetAddresses: 'tokenFaucetAddresses',
   usePools: 'usePools',
   prizePoolContractAddresses: 'prizePoolContractAddresses',
-  etherscanContractAbi: 'etherscanContractAbi',
-  useUserPoolData: 'useUserPoolData'
+  etherscanContractAbi: 'etherscanContractAbi'
 }
 
 export const ETHERSCAN_API_KEY = process.env.NEXT_JS_ETHERSCAN_API_KEY
