5742e7bca98997d97efdb6a7c8acdc163a175d5b
Dylan DesRosier <dylan@pooltogether.com>
2021-06-03 18:49:40 -0400

    add layout to all pages

--- pages/proposals/[id].jsx
+++ pages/proposals/[id].jsx
@@ -1,6 +1,11 @@
+import Layout from 'lib/components/Layout'
 import { ProposalUI } from 'lib/components/proposals/ProposalUI'
 import React from 'react'
 
 export default function IndexPage(props) {
-  return <ProposalUI />
+  return (
+    <Layout>
+      <ProposalUI />
+    </Layout>
+  )
 }
--- pages/proposals/create.jsx
+++ pages/proposals/create.jsx
@@ -1,6 +1,11 @@
+import Layout from 'lib/components/Layout'
 import { ProposalCreationUI } from 'lib/components/proposals/ProposalCreationUI'
 import React from 'react'
 
 export default function IndexPage(props) {
-  return <ProposalCreationUI />
+  return (
+    <Layout>
+      <ProposalCreationUI />
+    </Layout>
+  )
 }
