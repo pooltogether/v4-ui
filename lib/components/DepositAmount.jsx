baf9863f98c4cda4ae4e31828595dc501cfcbcc0
Charles Bergeron <chuck.bergeron@gmail.com>
2021-08-02 15:25:43 -0700

    * form stepper UI

--- lib/components/DepositAmount.jsx
+++ lib/components/DepositAmount.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import classnames from 'classnames'
 import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
 import { parseUnits } from '@ethersproject/units'
@@ -160,6 +161,8 @@ export const DepositAmount = (props) => {
         <div className='font-bold gradient-new text-center py-1 mt-4 text-xxs rounded-lg text-white'>
           {t('chancesToWinAreProportional')}
         </div>
+
+        <FormStepper activeStep={2} totalSteps={3} />
       </form>
     </>
   )
@@ -179,5 +182,95 @@ const DownArrow = (props) => {
         <path d='M7 1V12.17L2.12 7.29C1.73 6.9 1.09 6.9 0.700001 7.29C0.310001 7.68 0.310001 8.31 0.700001 8.7L7.29 15.29C7.68 15.68 8.31 15.68 8.7 15.29L15.29 8.7C15.68 8.31 15.68 7.68 15.29 7.29C14.9 6.9 14.27 6.9 13.88 7.29L9 12.17V1C9 0.45 8.55 0 8 0C7.45 0 7 0.45 7 1Z' />
       </svg>
     </div>
+  )
+}
+
+const FormStepper = (props) => {
+  const { totalSteps, activeStep } = props
+
+  const defaultCircleClassNames = 'border-2 h-4 w-4 rounded-full trans'
+  const activeCircleClassNames = 'bg-card border-highlight-1'
+  const inactiveCircleClassNames = 'bg-card-selected border-transparent'
+
+  const defaultLabelClassNames = 'font-inter text-xxs font-semibold mt-2 relative trans'
+  const activeLabelClassNames = 'text-highlight-1'
+  const inactiveLabelClassNames = 'text-primary-soft opacity-60'
+
+  const defaultBarClassNames = 'h-1 w-1/2 relative trans'
+  const activeBarClassNames = 'bg-highlight-1'
+  const inactiveBarClassNames = 'bg-transparent'
+
+  return (
+    <>
+      <div
+        className='bg-card-selected w-2/3 flex justify-between items-center mt-10 relative mx-auto'
+        style={{ height: 2 }}
+      >
+        <div className='w-full flex justify-between items-center absolute t-0 l-0 r-0 b-0 z-20'>
+          <div
+            className={classnames(defaultCircleClassNames, {
+              [activeCircleClassNames]: activeStep >= 1,
+              [inactiveCircleClassNames]: activeStep < 1
+            })}
+          ></div>
+          <div
+            className={classnames(defaultCircleClassNames, {
+              [activeCircleClassNames]: activeStep >= 2,
+              [inactiveCircleClassNames]: activeStep < 2
+            })}
+          ></div>
+          <div
+            className={classnames(defaultCircleClassNames, {
+              [activeCircleClassNames]: activeStep >= 3,
+              [inactiveCircleClassNames]: activeStep < 3
+            })}
+          ></div>
+        </div>
+
+        <div
+          className={classnames(defaultBarClassNames, {
+            [activeBarClassNames]: activeStep > 1,
+            [inactiveBarClassNames]: activeStep <= 1
+          })}
+          style={{ height: 2, left: 2 }}
+        ></div>
+        <div
+          className={classnames(defaultBarClassNames, {
+            [activeBarClassNames]: activeStep > 2,
+            [inactiveBarClassNames]: activeStep <= 2
+          })}
+          style={{ height: 2, right: 2 }}
+        ></div>
+      </div>
+      <div className='w-2/3 flex justify-between items-center mt-2 relative mx-auto'>
+        <div
+          className={classnames(defaultLabelClassNames, {
+            [activeLabelClassNames]: activeStep >= 1,
+            [inactiveLabelClassNames]: activeStep < 1
+          })}
+          style={{ left: -34 }}
+        >
+          Allow USDC
+        </div>
+        <div
+          className={classnames(defaultLabelClassNames, {
+            [activeLabelClassNames]: activeStep >= 2,
+            [inactiveLabelClassNames]: activeStep < 2
+          })}
+          style={{ right: -10 }}
+        >
+          Deposit USDC
+        </div>
+        <div
+          className={classnames(defaultLabelClassNames, {
+            [activeLabelClassNames]: activeStep >= 3,
+            [inactiveLabelClassNames]: activeStep < 3
+          })}
+          style={{ right: -44 }}
+        >
+          Confirm Order
+        </div>
+      </div>
+    </>
   )
 }
