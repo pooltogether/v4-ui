18655f5686b3a904fb44be6cc8fcc86ea9eb4d5d
Charles Bergeron <chuck.bergeron@gmail.com>
2021-10-11 21:53:32 -0700

    * adding sentry

--- lib/components/CustomErrorBoundary.jsx
+++ lib/components/CustomErrorBoundary.jsx
@@ -5,16 +5,16 @@ import { useOnboard } from '@pooltogether/bnc-onboard-hooks'
 import { ErrorPage } from 'lib/views/ErrorPage'
 
 class ErrorBoundary extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError (error) {
     return { hasError: true }
   }
 
-  render() {
+  render () {
     if (this.state.hasError) {
       return <ErrorPage />
     }
@@ -23,7 +23,7 @@ class ErrorBoundary extends React.Component {
   }
 }
 
-export function CustomErrorBoundary(props) {
+export function CustomErrorBoundary (props) {
   const { children } = props
   const { walletName } = useOnboard()
 
