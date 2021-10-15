664068165000f21957c7ad751783bbfa387b43ae
Charles Bergeron <chuck.bergeron@gmail.com>
2021-10-14 19:53:16 -0700

    * 1.0.0

--- lib/constants/prizeDistributors.ts
+++ lib/constants/prizeDistributors.ts
@@ -3,8 +3,8 @@ import { NETWORK } from '@pooltogether/utilities'
 
 export const PRIZE_DISTRIBUTORS = Object.freeze({
   [APP_ENVIRONMENTS.mainnets]: {
-    [NETWORK.mainnet]: '',
-    [NETWORK.polygon]: ''
+    [NETWORK.mainnet]: '0xb9a179DcA5a7bf5f8B9E088437B3A85ebB495eFe',
+    [NETWORK.polygon]: '0x8141BcFBcEE654c5dE17C4e2B2AF26B67f9B9056'
   },
   [APP_ENVIRONMENTS.testnets]: {
     [NETWORK.rinkeby]: ['0xfe6bc11738b4011bd59705335fd98fe066a38b51'],
--- lib/constants/prizePools.ts
+++ lib/constants/prizePools.ts
@@ -3,8 +3,8 @@ import { NETWORK } from '@pooltogether/utilities'
 
 export const PRIZE_POOLS = Object.freeze({
   [APP_ENVIRONMENTS.mainnets]: {
-    [NETWORK.mainnet]: '',
-    [NETWORK.polygon]: ''
+    [NETWORK.mainnet]: '0xd89a09084555a7D0ABe7B111b1f78DFEdDd638Be',
+    [NETWORK.polygon]: '0x19DE635fb3678D8B8154E37d8C9Cdf182Fe84E60'
   },
   [APP_ENVIRONMENTS.testnets]: {
     [NETWORK.rinkeby]: '0xbB8B489Aa452eBA03fe7B6AEe4946d984eAB3366',
