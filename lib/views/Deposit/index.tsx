c1156740245e0798a9faec7110bbcaf7b2699c2e
Charles Bergeron <chuck.bergeron@gmail.com>
2022-01-12 14:01:05 -0800

    * terms and disclaimer blurb

--- lib/views/Deposit/index.tsx
+++ lib/views/Deposit/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Trans } from 'react-i18next'
 
 import { DepositCard } from 'lib/views/Deposit/DepositCard'
 import { PagePadding } from 'lib/components/Layout/PagePadding'
@@ -9,6 +10,30 @@ export const DepositUI = () => {
     <PagePadding className='flex flex-col space-y-6'>
       <UpcomingPrizeCard className='mt-4' />
       <DepositCard />
+      <div className='text-center text-xxs opacity-50 dark:hover:opacity-70 hover:opacity-90 transition pt-4 xs:pt-6 px-4'>
+        <Trans
+          i18nKey='connectWalletTermsAndDisclaimerBlurb'
+          defaults={`By connecting a wallet, you agree to PoolTogether's <termsLink>Terms of Service</termsLink> and acknowledge that you have read and understand the <disclaimerLink>PoolTogether protocol disclaimer</disclaimerLink>`}
+          components={{
+            termsLink: (
+              <a
+                className='underline'
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://pooltogether.com/terms'
+              />
+            ),
+            disclaimerLink: (
+              <a
+                className='underline'
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://pooltogether.com/protocol-disclaimer'
+              />
+            )
+          }}
+        />
+      </div>
     </PagePadding>
   )
 }
