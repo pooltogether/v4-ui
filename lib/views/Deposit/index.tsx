ece5cb9686771ef5586205bc12162bf06868ce30
Charles Bergeron <chuck.bergeron@gmail.com>
2022-01-13 10:41:14 -0800

    * remove terms/etc from deposit page

--- lib/views/Deposit/index.tsx
+++ lib/views/Deposit/index.tsx
@@ -10,30 +10,6 @@ export const DepositUI = () => {
     <PagePadding className='flex flex-col space-y-6'>
       <UpcomingPrizeCard className='mt-4' />
       <DepositCard />
-      <div className='text-center text-xxs opacity-50 dark:hover:opacity-70 hover:opacity-90 transition pt-4 xs:pt-6 px-4'>
-        <Trans
-          i18nKey='connectWalletTermsAndDisclaimerBlurb'
-          defaults={`By connecting a wallet, you agree to PoolTogether's <termsLink>Terms of Service</termsLink> and acknowledge that you have read and understand the <disclaimerLink>PoolTogether protocol disclaimer</disclaimerLink>`}
-          components={{
-            termsLink: (
-              <a
-                className='underline'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://pooltogether.com/terms'
-              />
-            ),
-            disclaimerLink: (
-              <a
-                className='underline'
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://pooltogether.com/protocol-disclaimer'
-              />
-            )
-          }}
-        />
-      </div>
     </PagePadding>
   )
 }
