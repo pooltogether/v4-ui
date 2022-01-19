0bafcf2f539c2ee18aaa4a556d1e2a8a63657681
Charles Bergeron <chuck.bergeron@gmail.com>
2022-01-18 18:10:27 -0800

    * fix the return value of the sentry logging function

--- lib/services/sentryLog.ts
+++ lib/services/sentryLog.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/react'
 
-export const sentryLog = (msg: string, walletName?: string) => {
+export const sentryLog = (msg: string, walletName?: string): any => {
   Sentry.withScope(function (scope) {
     if (walletName) {
       scope.setTag('web3', walletName)
@@ -8,6 +8,7 @@ export const sentryLog = (msg: string, walletName?: string) => {
         name: walletName
       })
     }
+
     Sentry.captureMessage(msg)
   })
 }
