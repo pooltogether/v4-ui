769ad7d76f127ee2feb48e1f0296566349c819cb
Charles Bergeron <chuck.bergeron@gmail.com>
2022-03-03 09:39:13 -0800

    testnets account for testing pr #1849

--- src/hooks/useUsersAddress.ts
+++ src/hooks/useUsersAddress.ts
@@ -2,5 +2,6 @@ import { useOnboard } from '@pooltogether/bnc-onboard-hooks'
 
 export const useUsersAddress = () => {
   const { address } = useOnboard()
-  return address
+  return '0x856Bf74e6a12E398699d366d5C9F743854603e0D'
+  // return address
 }
