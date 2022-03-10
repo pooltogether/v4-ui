580625fc312776192ffe4ab6005a65e4798877f3
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-03-10 09:44:26 -0600

    npx prettier --write .

--- src/hooks/useUniswapSupportsNetwork.ts
+++ src/hooks/useUniswapSupportsNetwork.ts
@@ -1,8 +1,24 @@
-import { useOnboard } from "@pooltogether/bnc-onboard-hooks";
-import { NETWORK } from "@pooltogether/utilities";
-import { useMemo } from "react";
+import { useOnboard } from '@pooltogether/bnc-onboard-hooks'
+import { NETWORK } from '@pooltogether/utilities'
+import { useMemo } from 'react'
 
 export function useUniswapSupportsNetwork() {
-  const {  network: walletChainId } = useOnboard()
-return useMemo(() => [NETWORK.goerli,NETWORK.homestead,NETWORK.mainnet,NETWORK.kovan, NETWORK.kovan,NETWORK.matic,NETWORK.mumbai,NETWORK.optimism, NETWORK.polygon, NETWORK.rinkeby, NETWORK.ropsten].includes(walletChainId), [walletChainId])
+  const { network: walletChainId } = useOnboard()
+  return useMemo(
+    () =>
+      [
+        NETWORK.goerli,
+        NETWORK.homestead,
+        NETWORK.mainnet,
+        NETWORK.kovan,
+        NETWORK.kovan,
+        NETWORK.matic,
+        NETWORK.mumbai,
+        NETWORK.optimism,
+        NETWORK.polygon,
+        NETWORK.rinkeby,
+        NETWORK.ropsten
+      ].includes(walletChainId),
+    [walletChainId]
+  )
 }
