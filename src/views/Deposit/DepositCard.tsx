ee46a5dd8111d6c7151eacf8b88e37a16aa34c03
Dylan DesRosier <dylan@pooltogether.com>
2022-04-13 14:15:21 -0400

    Fix help button spacing

--- src/views/Deposit/DepositCard.tsx
+++ src/views/Deposit/DepositCard.tsx
@@ -263,16 +263,11 @@ const HelpLink = () => {
       href='https://docs.pooltogether.com/how-to/how-to-deposit'
       target='_blank'
       rel='noreferrer noopener'
-      className='text-center text-xs text-inverse opacity-60 hover:opacity-100 transition-opacity xs:-ml-3 flex flex-col items-center xs:flex-row xs:space-x-2 space-y-1 xs:space-y-0 justify-between'
+      className='text-center text-xs text-inverse opacity-60 hover:opacity-100 transition-opacity xs:-ml-3 flex flex-col items-center xs:flex-row xs:space-x-2 space-y-1 xs:space-y-0 justify-between xs:justify-center xs:space-x-2'
       style={{ minWidth: BUTTON_MIN_WIDTH }}
     >
-      <FeatherIcon
-        icon={'help-circle'}
-        className='relative w-4 h-4 inline-block'
-        style={{ top: -2 }}
-      />
-
-      {t('help', 'Help')}
+      <FeatherIcon icon={'help-circle'} className='relative w-4 h-4 inline-block' />
+      <span>{t('help', 'Help')}</span>
     </a>
   )
 }
