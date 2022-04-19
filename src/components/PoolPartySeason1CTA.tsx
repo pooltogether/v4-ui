f28e818115f3e6f9bc04f8345e205a27fc023757
Charles Bergeron <chuck.bergeron@gmail.com>
2022-04-19 09:01:09 -0700

    fix bad link and add NFT campaign CTA to deposit page

--- src/components/PoolPartySeason1CTA.tsx
+++ src/components/PoolPartySeason1CTA.tsx
@@ -7,7 +7,7 @@ export const PoolPartySeason1CTA = () => {
   return (
     <div className='block flex items-center p-2 justify-center rounded-lg space-x-2 bg-pt-purple-lightest dark:bg-pt-purple-dark border-2 border-pt-purple-lightest dark:border-pt-purple-dark text-xxs font-semibold mb-10'>
       <div>
-        <a target='_blank' href='https://pooltogether.com/pool-party'>
+        <a target='_blank' href='https://pooltogether.com/pool-party/season1'>
           <img className='w-20 xs:w-10' src={PoolPartySeason1Nft7} />
         </a>
       </div>
@@ -20,7 +20,7 @@ export const PoolPartySeason1CTA = () => {
 
           <a
             target='_blank'
-            href='https://pooltogether.com/pool-party'
+            href='https://pooltogether.com/pool-party/season1'
             className='flex items-center underline text-pt-purple dark:text-pt-teal dark:hover:text-white hover:text-pt-purple-light'
           >
             Learn more <FeatherIcon icon='external-link' className='w-4 h-4 ml-1' />
--- src/views/Deposit/index.tsx
+++ src/views/Deposit/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { PoolPartySeason1CTA } from '@components/PoolPartySeason1CTA'
 import { DepositCard } from '@views/Deposit/DepositCard'
 import { PagePadding } from '@components/Layout/PagePadding'
 import { UpcomingPrizeCard } from './UpcomingPrizeCard'
@@ -8,6 +9,7 @@ import { VotingPromptCard } from '@components/VotingPromptCard'
 export const DepositUI = () => {
   return (
     <PagePadding className='flex flex-col space-y-6'>
+      <PoolPartySeason1CTA />
       <UpcomingPrizeCard className='mt-4' />
       <DepositCard />
       <VotingPromptCard />
