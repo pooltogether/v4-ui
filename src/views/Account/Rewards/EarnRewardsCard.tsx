6510daa20a92bb92e30ce0f85d1fc262c6ec2051
Charles Bergeron <chuck.bergeron@gmail.com>
2022-06-02 16:28:23 -0700

    format the earn rewards card well

--- src/views/Account/Rewards/EarnRewardsCard.tsx
+++ src/views/Account/Rewards/EarnRewardsCard.tsx
@@ -1,23 +1,23 @@
 import { useUsersAddress } from '@pooltogether/wallet-connection'
 import { useTranslation } from 'react-i18next'
 import { NetworkIcon, TokenIcon } from '@pooltogether/react-components'
+import { useNetworkHexColor, useTokenBalance } from '@pooltogether/hooks'
 import { useAllChainsFilteredPromotions } from '@hooks/v4/TwabRewards/useAllChainsFilteredPromotions'
+import { getNetworkNameAliasByChainId } from '@pooltogether/utilities'
 
 // import { POOLStakingCards } from './POOLStakingCards'
+
+import { CHAIN_ID } from '@constants/misc'
 import { LoadingList } from '@components/PrizePoolDepositList/LoadingList'
 import { CardTitle } from '@components/Text/CardTitle'
 
 export const EarnRewardsCard = () => {
-  const usersAddress = useUsersAddress()
   const { t } = useTranslation()
 
   const queryResults = useAllChainsFilteredPromotions()
 
   const isFetched = queryResults.every((queryResult) => queryResult.isFetched)
   const isError = queryResults.map((queryResult) => queryResult.isError).filter(Boolean)?.length > 0
-  console.log(queryResults)
-  console.log(queryResults.map((queryResult) => queryResult.isError))
-  console.log({ isError })
 
   return (
     <div className='flex flex-col space-y-2'>
@@ -41,7 +41,9 @@ export const EarnRewardsCard = () => {
       )}
 
       {queryResults.map((queryResult) => {
-        return <ChainPromotions queryResult={queryResult} />
+        const { data } = queryResult || {}
+        const { chainId } = data || {}
+        return <ChainPromotions key={`chain-promotions-${chainId}`} queryResult={queryResult} />
       })}
 
       {/* <POOLStakingCards /> */}
@@ -51,11 +53,9 @@ export const EarnRewardsCard = () => {
 
 const ChainPromotions = (props) => {
   const { queryResult } = props
-  console.log({ queryResult })
 
   const { data } = queryResult
   const { chainId, promotions } = data || {}
-  console.log({ promotions })
 
   if (!promotions) {
     return null
@@ -74,19 +74,40 @@ const PromotionCard = (props) => {
   const { promotion, chainId } = props
   console.log(promotion)
 
+  const backgroundColor = useNetworkHexColor(chainId)
+  const usersAddress = useUsersAddress()
+  const { data: tokenBalance, isFetched } = useTokenBalance(chainId, usersAddress, promotion.token)
+  const { name, symbol } = tokenBalance || {}
+  console.log(tokenBalance)
+  console.log(CHAIN_ID)
+
   return (
-    <>
-      <div>promotionId: {promotion.id}</div>
-      <div>token: {promotion.token}</div>
-
-      <NetworkIcon chainId={chainId} className='mr-2' sizeClassName='w-4 h-4' />
-
-      <TokenIcon
-        sizeClassName='w-6 h-6'
-        className='mr-2'
-        chainId={chainId}
-        address={promotion.token}
-      />
-    </>
+    <div className='rounded-xl text-white p-6' style={{ backgroundColor, minHeight: 100 }}>
+      {/* <div>promotionId: {promotion.id}</div> */}
+      <div className='flex items-center justify-between font-bold'>
+        <div className='flex items-center'>
+          Earn{' '}
+          <TokenIcon
+            sizeClassName='w-4 h-4'
+            className='ml-2 mr-1'
+            chainId={chainId}
+            address={promotion.token}
+          />{' '}
+          {symbol} on{' '}
+          <span className='ml-1 capitalize'>{getNetworkNameAliasByChainId(chainId)}</span>
+        </div>
+        <div>
+          <NetworkIcon
+            chainId={chainId}
+            className='mr-2 border border-pt-purple-darker'
+            sizeClassName='w-8 h-8'
+          />
+        </div>
+      </div>
+      <div className='w-3/4'>
+        $500,000 of AVAX token rewards will be provided over the next 14 days to all depositors on
+        Avalanche.
+      </div>
+    </div>
   )
 }
