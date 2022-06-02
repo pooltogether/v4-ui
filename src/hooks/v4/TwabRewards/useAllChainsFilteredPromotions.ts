5824d7894abd786c9d5feb7fe4434a1219dd2b2f
Charles Bergeron <chuck.bergeron@gmail.com>
2022-06-02 15:12:00 -0700

    get to a point where we can display promotions by chain

---
+++ src/hooks/v4/TwabRewards/useAllChainsFilteredPromotions.ts
@@ -0,0 +1,73 @@
+import { useQueries } from 'react-query'
+import { GraphQLClient } from 'graphql-request'
+import gql from 'graphql-tag'
+
+import { CHAIN_ID } from '@constants/misc'
+import { NO_REFETCH } from '@constants/query'
+import { useSupportedTwabRewardsChainIds } from '@hooks/v4/TwabRewards/useSupportedTwabRewardsChainIds'
+import { useTwabRewardsSubgraphClient } from '@hooks/v4/TwabRewards/useTwabRewardsSubgraphClient'
+
+const FILTERS = {
+  [CHAIN_ID.rinkeby]: [],
+  [CHAIN_ID.mumbai]: [],
+  [CHAIN_ID.fuji]: [],
+  [CHAIN_ID.avalanche]: [],
+  [CHAIN_ID.mainnet]: [],
+  [CHAIN_ID.polygon]: ['0x1', '0x5']
+}
+
+/**
+ * Fetch all chain's promotions that have been 'allow listed'
+ * @param usersAddress
+ * @returns
+ */
+export const useAllChainsFilteredPromotions = () => {
+  const chainIds = useSupportedTwabRewardsChainIds()
+
+  return useQueries(
+    chainIds.map((chainId) => {
+      const client = useTwabRewardsSubgraphClient(chainId)
+
+      return {
+        ...NO_REFETCH,
+        queryKey: getChainFilteredPromotionsKey(chainId),
+        queryFn: async () => getChainFilteredPromotions(chainId, client),
+        enabled: Boolean(chainId)
+      }
+    })
+  )
+}
+
+const getChainFilteredPromotionsKey = (chainId: number) => ['getChainFilteredPromotions', chainId]
+
+export const getChainFilteredPromotions = async (chainId: number, client: GraphQLClient) => {
+  const query = promotionsQuery()
+  const variables = { ids: FILTERS[chainId] }
+
+  const promotionsResponse = await client.request(query, variables).catch((e) => {
+    console.error(e.message)
+    throw e
+  })
+  const { promotions } = promotionsResponse || {}
+
+  return { chainId, promotions }
+}
+
+const promotionsQuery = () => {
+  return gql`
+    query promotionsQuery($ids: [String!]!) {
+      promotions(where: { id_in: $ids }) {
+        id
+        creator
+        createdAt
+        startTimestamp
+        numberOfEpochs
+        epochDuration
+        tokensPerEpoch
+        rewardsUnclaimed
+        token
+        ticket
+      }
+    }
+  `
+}
--- src/views/Account/Rewards/EarnRewardsCard.tsx
+++ src/views/Account/Rewards/EarnRewardsCard.tsx
@@ -1,25 +1,92 @@
-import { CardTitle } from '@components/Text/CardTitle'
 import { useUsersAddress } from '@pooltogether/wallet-connection'
 import { useTranslation } from 'react-i18next'
-import { useAllChainFilteredPromotions } from '@hooks/v4/TwabRewards/useAllChainFilteredPromotions'
+import { NetworkIcon, TokenIcon } from '@pooltogether/react-components'
+import { useAllChainsFilteredPromotions } from '@hooks/v4/TwabRewards/useAllChainsFilteredPromotions'
+
 // import { POOLStakingCards } from './POOLStakingCards'
+import { LoadingList } from '@components/PrizePoolDepositList/LoadingList'
+import { CardTitle } from '@components/Text/CardTitle'
 
 export const EarnRewardsCard = () => {
   const usersAddress = useUsersAddress()
   const { t } = useTranslation()
 
-  const chainIds = useAllChainFilteredPromotions()
+  const queryResults = useAllChainsFilteredPromotions()
 
-  console.log({ chainIds })
+  const isFetched = queryResults.every((queryResult) => queryResult.isFetched)
+  const isError = queryResults.map((queryResult) => queryResult.isError).filter(Boolean)?.length > 0
+  console.log(queryResults)
+  console.log(queryResults.map((queryResult) => queryResult.isError))
+  console.log({ isError })
 
   return (
     <div className='flex flex-col space-y-2'>
       <CardTitle
         title={t('earnRewards')}
         // secondary={`$${amount.amountPretty}`}
-        // loading={!isFetched}
+        loading={!isFetched}
       />
+
+      {!isFetched && (
+        <LoadingList
+          listItems={1}
+          bgClassName='bg-pt-purple-lightest dark:bg-opacity-40 dark:bg-pt-purple'
+        />
+      )}
+
+      {isError && (
+        <div>
+          Unable to fetch rewards data due to subgraph issue, come back to check your rewards later!
+        </div>
+      )}
+
+      {queryResults.map((queryResult) => {
+        return <ChainPromotions queryResult={queryResult} />
+      })}
+
       {/* <POOLStakingCards /> */}
     </div>
+  )
+}
+
+const ChainPromotions = (props) => {
+  const { queryResult } = props
+  console.log({ queryResult })
+
+  const { data } = queryResult
+  const { chainId, promotions } = data || {}
+  console.log({ promotions })
+
+  if (!promotions) {
+    return null
+  }
+
+  return promotions.map((promotion) => (
+    <PromotionCard
+      key={`pcard-${chainId}-${promotion.id}`}
+      promotion={promotion}
+      chainId={chainId}
+    />
+  ))
+}
+
+const PromotionCard = (props) => {
+  const { promotion, chainId } = props
+  console.log(promotion)
+
+  return (
+    <>
+      <div>promotionId: {promotion.id}</div>
+      <div>token: {promotion.token}</div>
+
+      <NetworkIcon chainId={chainId} className='mr-2' sizeClassName='w-4 h-4' />
+
+      <TokenIcon
+        sizeClassName='w-6 h-6'
+        className='mr-2'
+        chainId={chainId}
+        address={promotion.token}
+      />
+    </>
   )
 }
