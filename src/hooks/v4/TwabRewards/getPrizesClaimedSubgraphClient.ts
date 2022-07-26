e791eed7fcb8a8d7fc76e89bef5c6f54b0875a64
Charles Bergeron <chuck.bergeron@gmail.com>
2022-07-26 16:58:39 -0700

    testnet subgraphs

--- src/hooks/v4/TwabRewards/getPrizesClaimedSubgraphClient.ts
+++ src/hooks/v4/TwabRewards/getPrizesClaimedSubgraphClient.ts
@@ -7,7 +7,13 @@ const PRIZES_CLAIMED_SUBGRAPH_URIS = {
   [CHAIN_ID.optimism]: `https://api.thegraph.com/subgraphs/name/pooltogether/optimism-v4-prizes-claimed`,
   [CHAIN_ID.mainnet]: `https://api.thegraph.com/subgraphs/name/pooltogether/mainnet-v4-prizes-claimed`,
   [CHAIN_ID.polygon]: `https://api.thegraph.com/subgraphs/name/pooltogether/polygon-v4-prizes-claimed`,
-  [CHAIN_ID.avalanche]: `https://api.thegraph.com/subgraphs/name/pooltogether/avalanche-v4-prizes-claimed`
+  [CHAIN_ID.avalanche]: `https://api.thegraph.com/subgraphs/name/pooltogether/avalanche-v4-prizes-claimed`,
+  [CHAIN_ID.rinkeby]: `https://api.thegraph.com/subgraphs/name/pooltogether/rinkeby-v4-prizes-claimed`,
+  [CHAIN_ID.mumbai]: `https://api.thegraph.com/subgraphs/name/pooltogether/mumbai-v4-prizes-claimed`,
+  [CHAIN_ID.fuji]: `https://api.thegraph.com/subgraphs/name/pooltogether/fuji-v4-prizes-claimed`,
+  [CHAIN_ID[
+    'optimism-kovan'
+  ]]: `https://api.thegraph.com/subgraphs/name/pooltogether/optimism-kovan-v4-prizes-claimed`
 }
 
 export const getPrizesClaimedSubgraphUri = (chainId) => {
