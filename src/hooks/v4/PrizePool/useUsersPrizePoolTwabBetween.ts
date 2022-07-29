96c4bb4fc5b9a037edd81a64a0a262081acd1794
Charles Bergeron <chuck.bergeron@gmail.com>
2022-07-29 16:01:51 -0700

    finish fixing rewards balance estimates

---
+++ src/hooks/v4/PrizePool/useUsersPrizePoolTwabBetween.ts
@@ -0,0 +1,72 @@
+import { sToMs } from '@pooltogether/utilities'
+import { PrizePool } from '@pooltogether/v4-client-js'
+import { useQuery } from 'react-query'
+
+import { useSelectedPrizePoolTicketDecimals } from '@hooks/v4/PrizePool/useSelectedPrizePoolTicketDecimals'
+import { getAmountFromBigNumber } from '@utils/getAmountFromBigNumber'
+
+/**
+ * Fetches a users TWAB between a set of two timestamps
+ * @param usersAddress
+ * @param prizePool
+ * @param startTimestamp
+ * @param endTimestamp
+ * @returns
+ */
+export const useUsersPrizePoolTwabBetween = (
+  usersAddress: string,
+  prizePool: PrizePool,
+  startTimestamp: number,
+  endTimestamp: number
+) => {
+  const { data: ticketDecimals, isFetched: isTicketDecimalsFetched } =
+    useSelectedPrizePoolTicketDecimals()
+
+  const enabled = !!usersAddress && !!startTimestamp && !!endTimestamp && isTicketDecimalsFetched
+
+  return useQuery(
+    getUsersPrizePoolTwabBetweenKey(usersAddress, prizePool, startTimestamp),
+    async () =>
+      getUserPrizePoolTwabBetween(
+        prizePool,
+        usersAddress,
+        ticketDecimals,
+        startTimestamp,
+        endTimestamp
+      ),
+    {
+      refetchInterval: sToMs(15),
+      keepPreviousData: true,
+      enabled
+    }
+  )
+}
+
+export const getUsersPrizePoolTwabBetweenKey = (
+  usersAddress: string,
+  prizePool: PrizePool,
+  startTimestamp: number
+) => ['useUsersPrizePoolTwab', usersAddress, prizePool?.id(), startTimestamp]
+
+export const getUserPrizePoolTwabBetween = async (
+  prizePool: PrizePool,
+  usersAddress: string,
+  decimals: string,
+  startTimestamp: number,
+  endTimestamp: number
+) => {
+  const twabUnformatted = await prizePool.getUsersAverageBalanceBetween(
+    usersAddress,
+    startTimestamp,
+    endTimestamp
+  )
+
+  const twab = getAmountFromBigNumber(twabUnformatted, decimals)
+
+  return {
+    prizePoolId: prizePool.id(),
+    chainId: prizePool.chainId,
+    usersAddress,
+    twab
+  }
+}
--- src/hooks/v4/TwabRewards/promotionHooks.ts
+++ src/hooks/v4/TwabRewards/promotionHooks.ts
@@ -5,7 +5,7 @@ import { formatUnits } from '@ethersproject/units'
 import { useToken } from '@pooltogether/hooks'
 
 import { Promotion, Epoch } from '@interfaces/promotions'
-import { useUsersPrizePoolTwabAtTimestamp } from '@hooks/v4/PrizePool/useUsersPrizePoolTwabAtTimestamp'
+import { useUsersPrizePoolTwabBetween } from '@hooks/v4/PrizePool/useUsersPrizePoolTwabBetween'
 import { useChainPrizePoolTicketTotalSupply } from '@hooks/v4/PrizePool/useChainPrizePoolTicketTotalSupply'
 
 export const useNextRewardIn = (promotion: Promotion) => {
@@ -49,21 +49,13 @@ export const useUsersCurrentEpochEstimateAccrued = (prizePool: PrizePool, promot
   const usersAddress = useUsersAddress()
   const currentEpoch = promotion.epochCollection.remainingEpochsArray?.[0]
   const epochStartTimestamp = currentEpoch?.epochStartTimestamp
-  // console.log(epochStartTimestamp)
-  const timestamp = Math.round(msToS(Date.now()))
-  // console.log(timestamp)
-  const { data: twabData, isFetched: isTwabDataFetched } = useUsersPrizePoolTwabAtTimestamp(
+  const now = Math.round(msToS(Date.now()))
+  const { data: twabData, isFetched: isTwabDataFetched } = useUsersPrizePoolTwabBetween(
     usersAddress,
     prizePool,
-    epochStartTimestamp
+    epochStartTimestamp,
+    now
   )
-  // const { data: twabData, isFetched: isTwabDataFetched } = useUsersPrizePoolTwabAtTimestamp(
-  //   usersAddress,
-  //   prizePool,
-  //   epochStartTimestamp
-  // )
-  // console.log(twabData)
-  console.log(twabData?.twab?.amountUnformatted)
 
   let currentEpochAccrued
   if (currentEpoch && isTwabDataFetched && tokenIsFetched) {
@@ -78,10 +70,8 @@ export const useUsersCurrentEpochEstimateAccrued = (prizePool: PrizePool, promot
     const tokensPerEpochFormatted = formatUnits(promotion.tokensPerEpoch, token.decimals)
 
     const currentEpochTotalEstimate = usersChainTwabPercentage * parseFloat(tokensPerEpochFormatted)
-    // console.log(currentEpochTotalEstimate)
 
     currentEpochAccrued = epochElapsedPercent * currentEpochTotalEstimate
-    // console.log(currentEpochAccrued)
   }
 
   return currentEpochAccrued
