9410816a1a6e644aa3ecf86b4d117d3e8e20dfa5
Dylan DesRosier <dylan@pooltogether.com>
2022-09-21 14:25:30 -0400

    Add condiditional

--- src/hooks/v4/TwabRewards/usePromotionVAPR.ts
+++ src/hooks/v4/TwabRewards/usePromotionVAPR.ts
@@ -32,8 +32,9 @@ export const usePromotionVAPR = (promotion: Promotion): number => {
     const isReady =
       tokenPricesIsFetched && tokenIsFetched && totalTwabSupply && depositToken?.decimals
 
-    if (daysRemaining > 0 && isReady && tokenPrices) {
-      const promotionTokenUsd = tokenPrices[promotionTokenAddress].usd
+    if (daysRemaining > 0 && isReady && !!tokenPrices) {
+      console.log({ tokenPrices, promotionTokenAddress, depositToken })
+      const promotionTokenUsd = tokenPrices[promotionTokenAddress]?.usd
       const depositTokenUsd = 1 // assume 1 ptaUSDC equals $1 here
 
       const valuePerDay = promotionTokenValuePerDay(
