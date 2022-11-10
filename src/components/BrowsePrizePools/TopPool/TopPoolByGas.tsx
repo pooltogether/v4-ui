8b544ab89a86e0fc8ced6ebd63180ec697110f8a
Ncookiez <ncukiert@gmail.com>
2022-11-10 14:39:58 -0300

    Using CHAIN_ID

--- src/components/BrowsePrizePools/TopPool/TopPoolByGas.tsx
+++ src/components/BrowsePrizePools/TopPool/TopPoolByGas.tsx
@@ -1,6 +1,7 @@
 import { useSelectedPrizePoolAddress } from '@hooks/useSelectedPrizePoolAddress'
 import { usePrizePool } from '@hooks/v4/PrizePool/usePrizePool'
 import { PrizePool } from '@pooltogether/v4-client-js'
+import { CHAIN_ID } from '@pooltogether/wallet-connection'
 import { useTranslation } from 'next-i18next'
 import { TopPool } from '.'
 
@@ -14,7 +15,7 @@ export const TopPoolByGas: React.FC<{
 
   // This is currently hardcoded to Polygon's V4 USDC pool.
   // Once there are more pools per chain this will have to be refactored.
-  const prizePool = usePrizePool(137, '0x19DE635fb3678D8B8154E37d8C9Cdf182Fe84E60')
+  const prizePool = usePrizePool(CHAIN_ID.polygon, '0x19DE635fb3678D8B8154E37d8C9Cdf182Fe84E60')
 
   return (
     <TopPool
