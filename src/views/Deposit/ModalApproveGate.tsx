df2762b8c7901a5c8878ce79654bcf90097208ed
Ncookiez <ncukiert@gmail.com>
2022-12-20 13:51:04 -0300

    Restructuring

--- src/views/Deposit/ModalApproveGate.tsx
+++ src/views/Deposit/ModalApproveGate.tsx
@@ -1,44 +1,64 @@
 import { ModalInfoList } from '@components/InfoList'
 import { EstimatedDepositGasItems } from '@components/InfoList/EstimatedGasItem'
 import { TxButton } from '@components/Input/TxButton'
-import { Amount } from '@pooltogether/hooks'
+import { Amount, useTokenAllowance } from '@pooltogether/hooks'
 import {
   ButtonLink,
   ButtonTheme,
   ThemedClipSpinner,
-  ButtonRadius
+  ButtonRadius,
+  CheckboxInputGroup
 } from '@pooltogether/react-components'
 import {
   formatBlockExplorerTxUrl,
-  Transaction,
-  TransactionState
+  TransactionState,
+  useApproveErc20,
+  useTransaction,
+  useUsersAddress
 } from '@pooltogether/wallet-connection'
 import { DepositLowAmountWarning } from '@views/DepositLowAmountWarning'
 import classNames from 'classnames'
-import React from 'react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface ModalApproveGateProps {
   className?: string
   amountToDeposit?: Amount
   chainId: number
-  approveTx: Transaction
-  sendApproveTx: () => void
   connectWallet?: () => void
+  spenderAddress: string
+  tokenAddress: string
 }
 
 /**
- * TODO: Make max approval optional
  * @param props
  * @returns
  */
 export const ModalApproveGate = (props: ModalApproveGateProps) => {
-  const { className, chainId, approveTx, sendApproveTx, connectWallet, amountToDeposit } = props
-
+  const { className, amountToDeposit, chainId, connectWallet, spenderAddress, tokenAddress } = props
+  const usersAddress = useUsersAddress()
+  const { refetch: refetchTokenAllowance } = useTokenAllowance(
+    chainId,
+    usersAddress,
+    spenderAddress,
+    tokenAddress
+  )
+  const [approveTransactionId, setApproveTransactionId] = useState('')
+  const approveTransaction = useTransaction(approveTransactionId)
+  const _sendApproveTx = useApproveErc20(tokenAddress, spenderAddress, {
+    callbacks: { onSuccess: () => refetchTokenAllowance() }
+  })
+  const [isInfiniteApproval, setIsInfiniteApproval] = useState(true)
   const { t } = useTranslation()
 
-  if (approveTx?.state === TransactionState.pending) {
-    const blockExplorerUrl = formatBlockExplorerTxUrl(approveTx.response?.hash, chainId)
+  // TODO: send non-infinite approval if `isInfiniteApproval` is false
+  const sendApproveTx = async () => {
+    const transactionId = await _sendApproveTx()
+    setApproveTransactionId(transactionId)
+  }
+
+  if (approveTransaction?.state === TransactionState.pending) {
+    const blockExplorerUrl = formatBlockExplorerTxUrl(approveTransaction.response?.hash, chainId)
 
     return (
       <div className={classNames(className, 'flex flex-col')}>
@@ -64,6 +84,7 @@ export const ModalApproveGate = (props: ModalApproveGateProps) => {
     )
   }
 
+  // TODO: redo explanations now that users can opt out of infinite approvals
   return (
     <div className={classNames(className, 'flex flex-col')}>
       <div className='mx-4 text-inverse opacity-60'>
@@ -98,12 +119,24 @@ export const ModalApproveGate = (props: ModalApproveGateProps) => {
         radius={ButtonRadius.full}
         chainId={chainId}
         onClick={sendApproveTx}
-        state={approveTx?.state}
-        status={approveTx?.status}
+        state={approveTransaction?.state}
+        status={approveTransaction?.status}
         connectWallet={connectWallet}
       >
         {t('confirmApproval', 'Confirm approval')}
       </TxButton>
+      <div className='flex justify-end mt-2 mr-2 text-xxs'>
+        <CheckboxInputGroup
+          large
+          id='infinite-approval-toggle'
+          name='infinite-approval-toggle'
+          label={'Infinite approval'}
+          checked={isInfiniteApproval}
+          handleClick={() => {
+            setIsInfiniteApproval(!isInfiniteApproval)
+          }}
+        />
+      </div>
     </div>
   )
 }
