724a812f3f717bfcbb25d8a70b130ef1e10b3b65
Charles Bergeron <chuck.bergeron@gmail.com>
2023-11-14 10:46:19 -0800

    simply remove all the alert code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,7 @@
 import { PageHeader } from '@components/Layout/PageHeader'
-import { ExternalLink } from '@pooltogether/react-components'
 import classNames from 'classnames'
-import React, { useEffect, ReactNode } from 'react'
-import { toast } from 'sonner'
+import React from 'react'
 import { Navigation } from './Navigation'
-
-/**
- * Sonner Toasts
- */
 
 interface LayoutProps {
   className?: string
@@ -17,12 +11,6 @@ interface LayoutProps {
 
 const Layout = (props: LayoutProps) => {
   const { children, className, backgroundClassName } = props
-
-  // useEffect(() => {
-  //   temporaryAlerts.forEach((alert) => {
-  //     toast(alert.content, { id: alert.id })
-  //   })
-  // })
 
   return (
     <div
@@ -34,34 +22,9 @@ const Layout = (props: LayoutProps) => {
     >
       <PageHeader />
       <Navigation />
-      {/* <AlertBanners /> */}
       {children}
     </div>
   )
 }
 
-// Layout.defaultProps = {
-//   backgroundClassName:
-//     'bg-gradient-to-br from-pt-purple-lightest to-pt-purple-lighter dark:from-gradient-purple dark:to-pt-purple-darkest minimal-scrollbar'
-// }
-
 export default Layout
-
-// const temporaryAlerts: { id: string; content: ReactNode }[] = [
-//   {
-//     id: 'aave-issues-04-11-2023',
-//     content: (
-//       <span className='flex flex-col items-center text-center text-xxs'>
-//         Aave has temporarily paused deposits and withdrawals on some assets until{' '}
-//         <ExternalLink
-//           href='https://app.aave.com/governance/proposal/?proposalId=358'
-//           className='text-pt-teal transition hover:opacity-90 underline'
-//           underline
-//         >
-//           proposal 358
-//         </ExternalLink>{' '}
-//         passes, due to a security issue. While no funds are at risk, this temporarily halts deposits and withdrawals from the Optimism and Polygon prize pools until the proposal passes.{' '}
-//       </span>
-//     )
-//   }
-// ]
